2c446f3ccae270a91ce8a2c0ec0c2acb46aeefd3
EunjinWoo <esther0904@naver.com>
2025-06-06 12:58:16 +0900

    setting: CORS 설정 시큐리티에 반영

--- cloud/src/main/java/com/project/cloud/global/config/SecurityConfig.java
+++ cloud/src/main/java/com/project/cloud/global/config/SecurityConfig.java
@@ -6,8 +6,10 @@ import com.project.cloud.global.auth.service.AuthFacadeService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -37,7 +39,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(allowedUrls).permitAll()
